0de9ec852a078e1bc7d7b23c9a1d643b348ca31f
Onildo Aguiar <onildo.aguiar@gmail.com>
2017-02-18 20:44:24 -0200

    Lição 4 do Módulo 2 - Xamarin Android

---
+++ Xamarin Android - Lição 4/XamarinAndroidLicao4/XamarinAndroidLicao4/Capital.cs
@@ -0,0 +1,67 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.IO;
+using SQLite;
+
+namespace XamarinAndroidLicao4
+{
+    [Activity(Label = "Capital")]
+    public class Capital : Activity
+    {
+        double defaultValue;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Capital);
+
+            var path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            path = Path.Combine(path, "Base.db3");
+            var conn = new SQLiteConnection(path);
+
+            var elementos = from s in conn.Table<Informacao>() select s;
+            foreach (var fila in elementos)
+            {
+                Toast.MakeText(this, fila.ReceitasBrasil.ToString(), ToastLength.Short).Show();
+                Toast.MakeText(this, fila.ReceitasMexico.ToString(), ToastLength.Short).Show();
+                Toast.MakeText(this, fila.DespesasBrasil.ToString(), ToastLength.Short).Show();
+                Toast.MakeText(this, fila.DespesasMexico.ToString(), ToastLength.Short).Show();
+            }
+
+            Button btnSair = FindViewById<Button>(Resource.Id.btnSair);
+            EditText txtCapitalBrasil = FindViewById<EditText>(Resource.Id.txtCapitalBrasil);
+            EditText txtCapitalMexico = FindViewById<EditText>(Resource.Id.txtCapitalMexico);
+            ImageView imgBrasil = FindViewById<ImageView>(Resource.Id.bandeiraBrasil);
+            ImageView imgMexico = FindViewById<ImageView>(Resource.Id.bandeiraMexico);
+
+            btnSair.Click += delegate
+            {
+                try
+                {
+                    Process.KillProcess(Process.MyPid());
+                }
+                catch (System.Exception ex)
+                {
+                    Toast.MakeText(this, ex.Message, ToastLength.Short).Show();
+                }
+            };
+
+            try
+            {
+                txtCapitalBrasil.Text = Intent.GetDoubleExtra("capitalBrasil", defaultValue).ToString();
+                txtCapitalMexico.Text = Intent.GetDoubleExtra("capitalMexico", defaultValue).ToString();
+                imgBrasil.SetImageResource(Resource.Drawable.bandeira_brasil);
+                imgMexico.SetImageResource(Resource.Drawable.bandeira_mexico);
+
+
+            }
+            catch (System.Exception ex)
+            {
+                Toast.MakeText(this, ex.Message, ToastLength.Short).Show();
+            }
+        }
+    }
+}
+
+
---
+++ Xamarin Android - Lição 4/XamarinAndroidLicao4/XamarinAndroidLicao4/MainActivity.cs
@@ -0,0 +1,91 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using Microsoft.Azure.Mobile;
+using Microsoft.Azure.Mobile.Analytics;
+using Microsoft.Azure.Mobile.Crashes;
+using System.Collections.Generic;
+using Android.Content;
+using System.IO;
+using SQLite;
+
+namespace XamarinAndroidLicao4
+{
+    [Activity(Label = "Calcular Capital", MainLauncher = true, Icon = "@drawable/icon", WindowSoftInputMode = SoftInput.AdjustPan)]
+    public class MainActivity : Activity
+    {
+        double capitalBrasil, capitalMexico;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Mobile Center Call
+            MobileCenter.Start("ec51b039-2f80-4948-b573-c5831148e177",
+                    typeof(Analytics), typeof(Crashes)); MobileCenter.Start("ec51b039-2f80-4948-b573-c5831148e177",
+                     typeof(Analytics), typeof(Crashes));
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            path = Path.Combine(path, "Base.db3");
+            var conn = new SQLiteConnection(path);
+            conn.CreateTable<Informacao>();
+
+            Button btnCalcular = FindViewById<Button>(Resource.Id.btnCalcular);
+            EditText txtRBrasil = FindViewById<EditText>(Resource.Id.txtrbrasil);
+            EditText txtRMexico = FindViewById<EditText>(Resource.Id.txtrmexico);
+            EditText txtDBrasil = FindViewById<EditText>(Resource.Id.txtdbrasil);
+            EditText txtDMexico = FindViewById<EditText>(Resource.Id.txtdmexico);
+
+            double rBrasil, rMexico, dBrasil, dMexico;
+
+            btnCalcular.Click += delegate
+            {
+                try
+                {
+                    rBrasil = double.Parse(txtRBrasil.Text);
+                    rMexico = double.Parse(txtRMexico.Text);
+                    dBrasil = double.Parse(txtDBrasil.Text);
+                    dMexico = double.Parse(txtDMexico.Text);
+                    capitalBrasil = rBrasil - dBrasil;
+                    capitalMexico = rMexico - dMexico;
+
+                    var Insertar = new Informacao();
+                    Insertar.ReceitasBrasil = rBrasil;
+                    Insertar.ReceitasMexico = rMexico;
+                    Insertar.DespesasBrasil = dBrasil;
+                    Insertar.DespesasMexico = dMexico;
+                    conn.Insert(Insertar);
+
+                    Toast.MakeText(this, "Salvando no SQLite", ToastLength.Long).Show();
+                    Analytics.TrackEvent("Button Clicked", new Dictionary<string, string> { { "Category", "Button Clicked" }, { "Button Name", "Calcular" } });
+                    Carregar();
+                }
+                catch (System.Exception ex)
+                {
+                    Toast.MakeText(this, ex.Message, ToastLength.Short).Show();
+                }
+            };
+        }
+
+        public void Carregar()
+        {
+            Intent objIntent = new Intent(this, typeof(Capital));
+            objIntent.PutExtra("capitalBrasil", capitalBrasil);
+            objIntent.PutExtra("capitalMexico", capitalMexico);
+            StartActivity(objIntent);
+        }
+    }
+
+    public class Informacao
+    {
+        public double ReceitasBrasil { get; set; }
+        public double ReceitasMexico { get; set; }
+        public double DespesasBrasil { get; set; }
+        public double DespesasMexico { get; set; }
+    }
+}
+
