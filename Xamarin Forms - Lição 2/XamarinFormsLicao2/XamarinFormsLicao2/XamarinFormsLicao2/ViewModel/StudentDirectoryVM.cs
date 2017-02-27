5f7d45e45cd10928efd56d7009e410d67a350992
Onildo Aguiar <onildo.aguiar@gmail.com>
2017-02-27 12:26:06 -0300

    Modelo Vista - Vista Modelo (MVVM).

---
+++ Xamarin Forms - Lição 2/XamarinFormsLicao2/XamarinFormsLicao2/XamarinFormsLicao2/ViewModel/StudentDirectoryVM.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsLicao2
+{
+    public class StudentDirectoryVM
+    {
+        public ObservableCollection<Student> Students { get; set; }
+
+        bool isBusy = false;
+        public bool IsBusy
+        {
+            get{ return isBusy;}
+            set { isBusy = value; }
+        }
+
+        public Command LoadDirectoryCommand { get; set;}
+
+        public StudentDirectoryVM()
+        {
+            Students = new ObservableCollection<Student>();
+            IsBusy = false;
+            LoadDirectoryCommand = new Command((obj) => LoadDirectory());
+        }
+
+        async void LoadDirectory()
+        {
+            if (!IsBusy)
+            {
+                IsBusy = true;
+
+                await Task.Delay(3000);
+
+                var loadedDirectory = StudentDirectoryService.LoadStudentDirectory();
+
+                foreach (var student in loadedDirectory.Students)
+                {
+                    Students.Add(student);
+                }
+                IsBusy = false;
+            }
+        }
+    }
+}
